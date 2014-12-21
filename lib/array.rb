4d361786d9a6442425231ec42df90935f8291b06
elena <elena.15@hotmail.it>
2014-12-21 11:43:03 +0000

    fixed indentation

--- lib/array.rb
+++ lib/array.rb
@@ -1,11 +1,11 @@
 class Array
 
-	def new_inject (default_value = nil)
-		copy = self.dup
-		result = default_value || first
-		copy.shift if default_value == nil
-		copy.each { |element| result = yield(result, element) }
-		result
-	end
+  def new_inject (default_value = nil)
+    copy = self.dup
+    result = default_value || first
+    copy.shift if default_value == nil
+    copy.each { |element| result = yield(result, element) }
+    result
+  end
 
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,24 +2,24 @@ require 'array'
 
 describe Array do
 
-	context "The new inject: " do
-		
-		it "should add up all the number in the array" do
-			expect([1,2,3,4].new_inject {|result, element| result + element}).to eq(10)
-		end
+  context "The new inject: " do
+	
+    it "should add up all the number in the array" do
+      expect([1,2,3,4].new_inject { |result, element| result + element }).to eq(10)
+    end
 
-		it "should multiply all the number in the array" do
-			expect([1,2,3,4].new_inject {|result, element| result * element}).to eq(24)
-		end
+    it "should multiply all the number in the array" do
+      expect([1,2,3,4].new_inject { |result, element| result * element }).to eq(24)
+    end
 
-		it "should subtract all the number in the array" do
-			expect([1,2,3,4].new_inject {|result, element| result - element}).to eq(-8)
-		end
+    it "should subtract all the number in the array" do
+      expect([1,2,3,4].new_inject { |result, element| result - element }).to eq(-8)
+    end
 
-		it "should divide all the number in the array" do
-			expect([1,2,3,4].new_inject {|result, element| result / element}).to eq(0)
-		end
+    it "should divide all the number in the array" do
+      expect([1,2,3,4].new_inject { |result, element| result / element} ).to eq(0)
+    end
 
-	end
+  end
 	
 end
